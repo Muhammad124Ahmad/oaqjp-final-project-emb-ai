124e56dd2c289b763623f713e8b4e90d3a8f213c
muhammad1249ahmad <>
2025-08-02 01:23:11 +0500

    Did Static code analysis on server.py

--- server.py
+++ server.py
@@ -1,19 +1,24 @@
-from flask import Flask,render_template,request,jsonify
+"""Deploying Part of This App"""
+
+from flask import Flask,render_template,request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app=Flask(__name__)
 
 @app.route('/emotionDetector')
-def emotionDetector():
+def emotion_detection():
+    """"This Function Retrieve text f
+    rom html and returns result"""
     text=request.args.get('textToAnalyze')
     result=emotion_detector(text)
     string=''
     for key in result:
         string=string+f"<p><b>{key}</b>: {result[key]}</p>"
-    return string    
+    return string
 
 @app.route('/')
-def showPage():
+def show_page():
+    """This Function Renders HTML template"""
     return render_template("index.html")
 
 if __name__=="__main__":
