5fa6a99573e497efd3bae98cd82525c0d8ecad5e
muhammad1249ahmad <>
2025-08-01 23:10:47 +0500

    Added and Setup the BERT emotion detector through API

---
+++ emotion_detection.py
@@ -0,0 +1,29 @@
+import os
+import json
+from huggingface_hub import InferenceClient
+from dotenv import load_dotenv
+
+load_dotenv()
+HF_TOKEN = os.getenv("HF_TOKEN")
+MODEL = "j-hartmann/emotion-english-distilroberta-base"
+
+
+def emotion_detector(text_to_analyze):
+    client = InferenceClient(
+        provider="hf-inference",
+        api_key=HF_TOKEN,
+    )
+
+    results = client.text_classification(
+        text_to_analyze,
+        model=MODEL,
+    )
+
+    emotion_dict = {result["label"]: result["score"] for result in results}
+    #list has dmoinant emotion at top so....
+    emotion_dict["dominant_emotion"] = list
+    (emotion_dict.keys())[0]
+    return emotion_dict
+
+
+print(emotion_detector("I love books"))
